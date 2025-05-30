5bf7cd9515a94e5628f9451f1fcb34987bfe3eee
ledeunf <jfld791000@gmail.com>
2025-05-30 13:10:44 +0200

    cour jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
-import App from './App.jsx'
+import App from './oldApp2counter.jsx'
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
