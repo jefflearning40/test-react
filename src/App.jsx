950bf092b95d38ae8ff0f535c27163ab7201467a
ledeunf <jfld791000@gmail.com>
2025-05-31 12:20:11 +0200

    install react

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
-import {useState} from "react"
 
-function App(){
 
-  const [count,setCount] = useState(0)
 
-  const increment = () => {
-    if (count<10){
-      setCount(count +1)
-    }
-  }
-
-  const decrement = () => {
-    if (count>0){
-      setCount(count-1)
-    }
-  }
-
-  const raz= () => {
-   setCount(0)
-  }
-  
-  return <>
-
-<p>Compteur :{count}</p>
-<button onClick={increment}>Incrementer</button>
-<button onClick={decrement}>decrementer</button>
-<button onClick={raz}>RAZ</button>
-  </>
-
+function App() {
+  return 'Hello World'
 }
+
 export default App
