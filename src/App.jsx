d66d928fdbbf01d9f16f5003358c1df6caee9d48
jefflearning40 <jfld791000@gmail.com>
2025-06-01 11:26:03 +0200

    from my pc desk

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import React, { Fragment } from 'react';
-const Title='Bonjour'
-const Para="Tout le monde"
+
 function App() {
-  return (
-    <Fragment>
-      <h1 id="Title" className="title">{Title}</h1>
-      <p>{Para}</p>
-    </Fragment>
-  );
+ return 'Hello world'
 }
 
-export default App;
+export default App
