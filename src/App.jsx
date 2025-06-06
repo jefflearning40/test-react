e27e4d29faae8cda77433a18d44b5b020c6c31a9
ledeunf <jfld791000@gmail.com>
2025-06-06 11:29:48 +0200

    exo3style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import Message from "./Message";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const App = () => (
   <div>
     <Message text="Information système" type="info" />
     <Message text="Alerte de sécurité" type="alert" />
     <Message text="Connexion réussie" type="success" />
   </div>
 );
+
+export default App;
