033ed0e44e80817cd5d705b8a2ce606905d26933
jefflearning40 <jfld791000@gmail.com>
2025-06-14 12:37:28 +0200

    recup

---
+++ src/cour2.jsx
@@ -0,0 +1,18 @@
+import {useState} from "react"
+function coursA() {
+ const [person,setPerson] =useState({
+    firstName :"John",
+    lastName :"Doe",
+    age : 18
+ })
+ const [count,setCount] = useState(0)
+ const incrementAge = () => {
+    setPerson({...person, age: person.age +1})
+ }
+ return<>
+ <p>Age de {person.firstName} : {person.age}</p>
+ <button onClick={incrementAge}>Gagner une année</button>
+ </>
+}
+
+export default coursA;
