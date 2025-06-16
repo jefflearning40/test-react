8f64f41fc52e83aebcbf1796faaad55c24f2958c
ledeunf <jfld791000@gmail.com>
2025-06-16 11:06:23 +0200

    exo1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import React from "react";
+import ListeBouton from "./exo1";
 
 export default function App() {
-  // Liste des utilisateurs
-  const users = ["Alice", "Bob", "Charlie", "David", "Eve"];
 
-  // Fonction pour afficher une alerte avec le nom de l'utilisateur cliqué
-  const handleClick = (userName) => {
-    alert(`Utilisateur sélectionné : ${userName}`);
-  };
-
-  return (
-    <div>
-      <h1>Liste des utilisateurs</h1>
-      <div>
-        {users.map((user, index) => (
-          <button key={index} onClick={() => handleClick(user)}>
-            {user}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <ListeBouton/>
+        </div>
+    );
 }
--- src/exo1.jsx
+++ src/exo1.jsx
@@ -1,10 +1,10 @@
 
 
 export default function listebouton() {
-  // Liste des utilisateurs
-  const users = ["Alice", "Bob", "Charlie", "David", "Eve"];
+ 
+  const users = ["Nina", "Nino", "Allan", "Alvyn", "moi"];
 
-  // Fonction pour afficher une alerte avec le nom de l'utilisateur cliqué
+ 
   const handleClick = (userName) => {
     alert(`Utilisateur sélectionné : ${userName}`);
   };
