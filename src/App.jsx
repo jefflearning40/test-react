e3b282cbb7e521f3a86fbd49c6fa886044309195
ledeunf <jfld791000@gmail.com>
2025-06-17 10:26:49 +0200

    chucnorrois installed

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import ChuckButton from "./chucknorris/ChuckButton";
+import Joke from "./chucknorris/Joke";
+import "./chucknorris/App.scss";
+
+const App = () => {
+  const [jokeData, setJokeData] = useState("");
+
+  // --- Fonction qui récupère une blague
+  const fetchJoke = () => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setJokeData(data.value);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <main>
+      <header>
+        <img
+          src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small.png"
+          alt="CHUCK NORRIS"
+        />
+      </header>
+      <Joke jokeData={jokeData} />
+      <ChuckButton fetchJoke={fetchJoke} />
+    </main>
+  );
+};
+
+export default App;
--- src/chucknorris/chuckButton.jsx
+++ src/chucknorris/chuckButton.jsx
@@ -1,7 +1,7 @@
 // Fonction qui affiche un bouton, qui, va venir faire une requpete réseau pour récupérer la blague.
 // La fonction de requete réseau, est passée en props depuis le parent
 const ChuckButton = ({ fetchJoke }) => {
-  return <button onClick={fetchJoke}>{">Clique ici<"}</button>;
+  return <button onClick={fetchJoke}>{">Click here<"}</button>;
 };
 
 export default ChuckButton;
